5d207fdf1488b28df7fca1da215f52334bade042
AndriiA <rassor96@gmail.com>
2023-11-13 23:02:08 +0200

    AKUI-4: Replace common css on css-in-js

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -1,8 +1,27 @@
-import { clsx } from 'clsx';
+import { css, SerializedStyles } from '@emotion/react';
+import * as styles from '../../styles';
 import { TagProps } from './Tag.types';
 
-import styles from './Tag.module.scss';
+const colorsMap: Record<TagProps['color'], SerializedStyles> = {
+  blue: css`color: ${styles.color.carolinaBlue}; background-color: ${styles.color.lightSkyBlue_40};`,
+  green: css`color: ${styles.color.lightSeaGreen}; background-color: ${styles.color.lightSeaGreen_20};`,
+  orange: css`color: ${styles.color.tigersEye}; background-color: ${styles.color.tigersEye_20};`,
+  pink: css`color: ${styles.color.amethyst}; background-color: ${styles.color.amethyst_20};`,
+  purple: css`color: ${styles.color.telemagenta}; background-color: ${styles.color.telemagenta_20};`
+};
 
-export const Tag = ({ children, color }: TagProps) => (
-  <span className={clsx(styles.tag, styles[color])}>{children}</span>
-);
+export const Tag = ({ children, color }: TagProps) => {
+  const tagStyle = css`
+    padding: 7px 10px;
+    border-radius: 3px;
+    letter-spacing: 0.36px;
+    line-height: 100%;
+    font-size: ${styles.fontSize.micro};
+    font-weight: ${styles.fontWeight.medium};
+    ${colorsMap[color]}
+  `;
+
+  return (
+    <span css={tagStyle}>{children}</span>
+  );
+};
