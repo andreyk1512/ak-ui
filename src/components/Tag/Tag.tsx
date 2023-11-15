a51ee6401bc133fbe9d935c2d2c1ad6997f23d18
AndriiA <rassor96@gmail.com>
2023-11-15 02:26:41 +0200

    AKUI-5: Refactoring

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -1,28 +1,6 @@
-import { css, SerializedStyles } from '@emotion/react';
-import * as styles from '../../styles';
+import { tagStyles } from './Tag.styles';
 import { TagProps } from './Tag.types';
 
-const colorsMap: Record<TagProps['color'], SerializedStyles> = {
-  blue: css`color: ${styles.color.carolinaBlue}; background-color: ${styles.color.lightSkyBlue_40};`,
-  green: css`color: ${styles.color.lightSeaGreen}; background-color: ${styles.color.lightSeaGreen_20};`,
-  orange: css`color: ${styles.color.tigersEye}; background-color: ${styles.color.tigersEye_20};`,
-  pink: css`color: ${styles.color.amethyst}; background-color: ${styles.color.amethyst_20};`,
-  purple: css`color: ${styles.color.telemagenta}; background-color: ${styles.color.telemagenta_20};`
-};
-
 export const Tag = ({ children, color }: TagProps) => {
-  const tagStyle = css`
-    display: inline-flex;
-    padding: 7px 10px;
-    border-radius: 3px;
-    letter-spacing: 0.36px;
-    line-height: 100%;
-    font-size: ${styles.fontSize.micro};
-    font-weight: ${styles.fontWeight.medium};
-    ${colorsMap[color]}
-  `;
-
-  return (
-    <span css={tagStyle}>{children}</span>
-  );
+  return <span css={tagStyles(color)}>{children}</span>;
 };
--- src/styles.ts
+++ src/styles.ts
@@ -34,8 +34,8 @@ export const fontSize = {
 };
 
 export const fontWeight = {
-    regular: 400,
-    medium: 500,
-    semiBold: 600,
-    bold: 700,
+  regular: 400,
+  medium: 500,
+  semiBold: 600,
+  bold: 700,
 };
