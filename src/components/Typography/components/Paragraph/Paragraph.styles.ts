db1f7ee01e58915af8a954a9931935ca64721cca
andrii.krupa <akrupa1512@gmail.com>
2024-10-07 11:01:24 +0300

    AKUI-12: Remove margin from Typography.Title and Typography.Paragraph

--- src/components/Typography/components/Paragraph/Paragraph.styles.ts
+++ src/components/Typography/components/Paragraph/Paragraph.styles.ts
@@ -2,6 +2,7 @@ import { css } from '@emotion/react';
 import * as styles from '../../../../styles';
 
 export const paragraphStyles = css`
+  margin: 0;
   color: ${styles.color.uclaBlue};
   font-size: ${styles.fontSize.regular};
   font-weight: ${styles.fontWeight.regular};
